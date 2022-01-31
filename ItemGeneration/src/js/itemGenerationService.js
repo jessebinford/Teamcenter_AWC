ee0aa5f0bdb032dd2cc116f6425fcffd2e7e8c00
Jesse Binford <12979360+jessebinford@users.noreply.github.com>
2022-01-30 19:46:29 -0800

    Update itemGenerationService.js

--- ItemGeneration/src/js/itemGenerationService.js
+++ ItemGeneration/src/js/itemGenerationService.js
@@ -18,7 +18,10 @@ export let getPrimarySelectedObject = function( context ) {
 	// object in different places, based on what view you are in, we need to look at the selected and pselected
 	// object and determine which one we are supposed to have. I am doing this by simply checking the object type.
 	
-	// This is solely used for the pasting of the relation in this example.
+	// This is used for the pasting of the relation in this example, as well as getting the object name for copying
+	// it to the new object(s) we make.
+	
+	// Change ItemRevision in this logic if you want to tie this to a different object type.
 	
 	var requestObj;
 	
@@ -59,7 +62,7 @@ export let formatItemIdGenerationInput = function( data ) {
 	formattedGenerationArray.push(exampleInput);
 	
 	/*
-	    This example would queue up 10 id requests...
+	    This example would add 10 id requests...
 		
 	for( var ii = 0; ii < 10; ii++ ) {
 		var exampleInput = {
@@ -90,23 +93,30 @@ export let formatItemCreationInput = function( data ) {
 	
 	for ( var currentIdx = 0; currentIdx < data.generatedIdValues.length; currentIdx++ ) {
 		
+		// We grab the new item id value
 		var idValue = data.generatedIdValues[currentIdx].generatedValues.item_id.nextValue;
 		
+		// Here we configure all of the properties we want to set on the item object
 		var itemProperties = {
 			item_id: [ idValue ],
 			object_name: [ data.primarySelectedObject.props.object_name.dbValue ]
 		};
 		
+		// Here we configure all of the properties we want to set on the item revision object
 		var revisionProperties = {
 			item_revision_id: ["A"]
 		};
 		
+		// Now we map the revision properties to our rev definition.
+		// Change the ItemRevision value to your target object rev type (if you want a different type)
 		var revisionDefinition = [{
 			boName: "ItemRevision",
 			compoundCreateInput: {},
 			propertyNameValues: revisionProperties
 		}];
 		
+		// Now we map the item revision to the item definition, as well as the item properties.
+		// Change the Item value to your target object type (if you want a different type)
 		var itemDefinition = {
 			boName: "Item",
 			compoundCreateInput: {
@@ -115,6 +125,7 @@ export let formatItemCreationInput = function( data ) {
 			propertyNameValues: itemProperties
 		};
 		
+		// Now put everything together into the final object
 		var creationDefinition = {
 			clientId: "CreateObject",
 			createData: itemDefinition,
@@ -124,6 +135,7 @@ export let formatItemCreationInput = function( data ) {
 			workflowData: {}
 		};
 		
+		// Add new formatted object to the array for our SOA call
 		formattedGenerationArray.push( creationDefinition );
 		
 	}
@@ -134,6 +146,12 @@ export let formatItemCreationInput = function( data ) {
 
 
 export let parseCreatedObjectsForRevisions = function( response ) {
+	
+	// This function is used to parse the SOA response and return an array
+	// that only contains the item revision objects that we need in order
+	// to perform the attachment of the new objects to our parent object the
+	// button was called from.
+	
 	var resultValueSet = [];
 	var outputs = response.output;
 	
